6d1a0a32640478951741926b00328519bf5a7518
Jammer1510 <76716282+Jammer1510@users.noreply.github.com>
2022-09-16 03:32:55 -0700

    Add files via upload

--- Swing_project/src/swing_project/Swing_project.java
+++ Swing_project/src/swing_project/Swing_project.java
@@ -1,3 +1,16 @@
+/***************************************************************  
+*  file: DoubleIt.java  
+*  author: Timmy.Lin  
+*  class: CS 245 – User Interface  
+*  
+*  assignment: Swing project 1  
+*  date last modified: 9/12/2021  
+*  
+*  purpose: This program accepts integer input from the command line, doubles the  
+*  value of input; the result is output to both the screen and a file called data.txt  
+*  
+****************************************************************/  
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -18,3 +31,5 @@ public class Swing_project {
     }
     
 }
+
+
